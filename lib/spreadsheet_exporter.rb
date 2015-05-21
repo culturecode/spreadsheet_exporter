b13fe9756766cf3f2f55367146ee0a9543e3d9f3
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2015-05-21 14:37:01 -0700

    Automatically register the xlsx mime type if Mime::Type is defined

--- lib/spreadsheet_exporter.rb
+++ lib/spreadsheet_exporter.rb
@@ -1,5 +1,9 @@
 require 'spreadsheet_exporter/csv'
 require 'spreadsheet_exporter/xlsx'
-
 module SpreadsheetExporter
+  begin
+    Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
+  rescue NameError
+    # No Mime::Type
+  end
 end

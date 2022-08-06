001a54fd079e954501791831d178c96a7306f0b8
Alex Dunae <alex@dunae.ca>
2022-08-05 23:24:22 -0700

    Add as_json from ActiveSupport

--- lib/spreadsheet_exporter.rb
+++ lib/spreadsheet_exporter.rb
@@ -1,5 +1,8 @@
 require_relative './spreadsheet_exporter/csv'
 require_relative './spreadsheet_exporter/xlsx'
+require 'active_support'
+require 'active_support/core_ext/object/json'
+
 module SpreadsheetExporter
   begin
     Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx

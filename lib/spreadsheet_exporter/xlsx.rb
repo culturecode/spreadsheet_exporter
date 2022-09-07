a32e6c41ba342ef73d7e55f49e1ea1026561289f
Alex Dunae <alex@dunae.ca>
2022-09-07 12:42:43 -0700

    Docs

--- lib/spreadsheet_exporter/xlsx.rb
+++ lib/spreadsheet_exporter/xlsx.rb
@@ -38,7 +38,7 @@ module SpreadsheetExporter
 
       added_data_sources = add_data_sources(workbook, header_format, data_sources)
 
-      add_worksheet_validation(workbook, worksheet, column_indexes, added_data_sources, header_format, options)
+      add_worksheet_validation(workbook, worksheet, column_indexes, added_data_sources, header_format, validations)
 
       add_frozen_panes(worksheet, freeze_panes)
 
@@ -57,19 +57,22 @@ module SpreadsheetExporter
       worksheet.freeze_panes(Integer(rows), Integer(cols))
     end
 
-    # Write each data_source to the `data` worksheet and reference it with a named range
+    # Write each data_source to the `data` worksheet and wrap it with a named range so
+    # we can easily reference it later.
     #
-    # `data_sources` is a hash in the format
-    #
+    # `data_sources` is a hash in the format:
     #     { 'data_source_id' => ['data', 'source', 'options'] }
     #
-    # For data sources dependent on the value in another column, the format is
+    # This will create a named range called `data_source_id`.
     #
+    # For data sources dependent on the value in another column, the format is
     #     { 'data_source_id' => {
-    #         'other_col_val_1' => ['options', 'when', 'val is 1'],
-    #         'other_col_val_2' => ['options', 'when', 'val is 2']
+    #         'other_col_val_1' => ['options', 'when', 'val is val_1'],
+    #         'other_col_val_2' => ['options', 'when', 'val is val_2']
     #       }
     #     }
+    #
+    # This will create two named ranges: `data_source_id_val_1` and `data_source_id_val_2`.
     def self.add_data_sources(workbook, header_format, data_sources)
       return {} if data_sources.empty?
 
@@ -83,13 +86,14 @@ module SpreadsheetExporter
       column_index = 0
       data_sources.stringify_keys.each do |data_key, data_values|
         if data_values.is_a?(Hash)
-          # nested, conditional data structure
+          # this is a dependent data source
           data_values.each do |data_value, sub_values|
             sub_key = sanitize_defined_name("#{data_key}_#{data_value}")
             data_source_refs[sub_key] = add_data_source(workbook, data_sheet, sub_key, sub_values, column_index, header_format)
             column_index += 1
           end
         else
+          # this is an independent data source
           data_source_refs[data_key] = add_data_source(workbook, data_sheet, data_key, data_values, column_index, header_format)
           column_index += 1
         end
@@ -100,10 +104,9 @@ module SpreadsheetExporter
 
     # Write a data column to the `data` worksheet and define it as a named range
     #
-    # Returnd the named range's name
+    # Returns the named range's name
     def self.add_data_source(workbook, data_sheet, data_key, data_values, column_index, header_format)
       unless data_values.is_a?(Array)
-        debugger
         raise ArgumentError, "data_values should be an array (got #{data_values.inspect}"
       end
 
@@ -119,7 +122,7 @@ module SpreadsheetExporter
       data_key
     end
 
-    def self.add_worksheet_validation(workbook, worksheet, column_indexes, data_sources, header_format, validations)
+    def self.add_worksheet_validation(workbook, worksheet, column_indexes, added_data_sources, header_format, validations)
       return if validations.empty?
 
       validations.each do |column_name, column_validation|
@@ -135,7 +138,7 @@ module SpreadsheetExporter
                          parent_col = xl_col_to_name(parent_col_index, true)
                          dependent_named_range(column_validation.data_source, parent_col)
                        else
-                         data_sources[column_validation.data_source]
+                         added_data_sources[column_validation.data_source]
                        end
 
         unless defined_name
